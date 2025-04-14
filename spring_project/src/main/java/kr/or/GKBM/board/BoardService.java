eff300086fe87050b431ae2f5293293c6cfc0da8
DESKTOP-ETJ58KB\admin <dy8426@gmail.com>
2025-04-14 16:40:29 +0900

    0414 board

--- spring_project/src/main/java/kr/or/GKBM/board/BoardService.java
+++ spring_project/src/main/java/kr/or/GKBM/board/BoardService.java
@@ -1,5 +1,17 @@
 package kr.or.GKBM.board;
 
+import java.util.List;
+
+
 public interface BoardService {
 
+	List getBoardList();
+	
+	public int updateEmp(BoardDTO boardDTO);
+	public int insertEmp(BoardDTO boardDTO);
+	public int deleteEmp(BoardDTO boardDTO);
+	
+	public BoardDTO getBoardDetail(BoardDTO boardDTO);
+	public int viewsUpdateBoard(BoardDTO boardDTO);
+	
 }
--- spring_project/src/main/java/kr/or/GKBM/board/BoardServiceImpl.java
+++ spring_project/src/main/java/kr/or/GKBM/board/BoardServiceImpl.java
@@ -1,5 +1,43 @@
 package kr.or.GKBM.board;
 
-public interface BoardServiceImpl {
+import java.util.List;
+
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public List getBoardList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateEmp(BoardDTO boardDTO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertEmp(BoardDTO boardDTO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteEmp(BoardDTO boardDTO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public BoardDTO getBoardDetail(BoardDTO boardDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int viewsUpdateBoard(BoardDTO boardDTO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
